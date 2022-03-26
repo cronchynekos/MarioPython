4fd73b812297115f081a516236985eb3a7c34551
Nathan Kim <nateykim@gmail.com>
2022-03-25 17:15:31 -0700

    delete

--- venv/settings.py
+++
@@ -1,8 +0,0 @@
-class Settings():
-    def __init__(self):
-        self.screen_width = 1280
-        self.screen_height = 576
-        self.bg_color = (80, 156, 250)
-
-        self.mario_height = 32
-        self.mario_width = 32
